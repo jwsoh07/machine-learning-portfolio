4f46b009c9615698a3e7c9ec3a2fffb5fd4ea954
jwsoh07 <mail.junwen@gmail.com>
2024-05-10 10:43:54 +0800

    Implement basic moving average use case

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import streamlit_antd_components as sac
 
 # use cases
+import time_series_analysis.simple_moving_average
 import supervised_learning.kth_nearest_neighbours
 
 # Sidebar
@@ -9,6 +10,14 @@ with st.sidebar:
     st.subheader("Types of Machine Learning")
     st.session_state.selected_model = sac.menu(
         [
+            sac.MenuItem(
+                "Time Series Analysis",
+                children=[
+                    sac.MenuItem(
+                        "Moving Average",
+                    ),
+                ],
+            ),
             sac.MenuItem(
                 "Supervised Learning",
                 children=[
@@ -35,6 +44,9 @@ with st.sidebar:
             ),
         ]
     )
+
+if st.session_state.selected_model == "Moving Average":
+    time_series_analysis.simple_moving_average.artificial_use_case()
 
 if st.session_state.selected_model == "k-Nearest Neighbors (k-NN)":
     supervised_learning.kth_nearest_neighbours.iris_flower_classification()
