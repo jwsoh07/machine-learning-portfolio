edf292a3edd5960134da48b44b389ac84d826deb
jwsoh07 <mail.junwen@gmail.com>
2024-05-10 12:20:17 +0800

    Add menu item to access fraud detection use case

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import streamlit_antd_components as sac
 # use cases
 import time_series_analysis.moving_average
 import supervised_learning.kth_nearest_neighbours
+import supervised_learning.logistic_regression
 
 # Sidebar
 with st.sidebar:
@@ -23,6 +24,9 @@ with st.sidebar:
                 children=[
                     sac.MenuItem(
                         "k-Nearest Neighbors (k-NN)",
+                    ),
+                    sac.MenuItem(
+                        "Logistic Regression",
                     ),
                 ],
             ),
@@ -50,3 +54,6 @@ if st.session_state.selected_model == "Moving Average":
 
 if st.session_state.selected_model == "k-Nearest Neighbors (k-NN)":
     supervised_learning.kth_nearest_neighbours.iris_flower_classification()
+
+if st.session_state.selected_model == "Logistic Regression":
+    supervised_learning.logistic_regression.fraud_detection()
