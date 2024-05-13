21461081c3a0ab29fed4998a69899e661b9076ab
jwsoh07 <mail.junwen@gmail.com>
2024-05-13 18:47:51 +0800

    Update streamlit app sidebar

--- main.py
+++ main.py
@@ -1,58 +1,30 @@
 import streamlit as st
-import streamlit_antd_components as sac
+from streamlit_option_menu import option_menu
 
 # use cases
 import time_series_analysis.moving_average
 import supervised_learning.kth_nearest_neighbours
 import supervised_learning.logistic_regression
 
-# Sidebar
 with st.sidebar:
-    st.subheader("Types of Machine Learning")
-    st.session_state.selected_model = sac.menu(
+    selected = option_menu(
+        "ML Projects",
         [
-            sac.MenuItem(
-                "Time Series Analysis",
-                children=[
-                    sac.MenuItem(
-                        "Moving Average",
-                    ),
-                ],
-            ),
-            sac.MenuItem(
-                "Supervised Learning",
-                children=[
-                    sac.MenuItem(
-                        "k-Nearest Neighbors (k-NN)",
-                    ),
-                    sac.MenuItem(
-                        "Logistic Regression",
-                    ),
-                ],
-            ),
-            sac.MenuItem(
-                "Unsupervised Learning",
-                children=[
-                    sac.MenuItem(
-                        "Placeholder",
-                    ),
-                ],
-            ),
-            sac.MenuItem(
-                "Reinforcement Learning",
-                children=[
-                    sac.MenuItem(
-                        "Placeholder",
-                    ),
-                ],
-            ),
-        ]
+            "Credit Card Fraud Detection",
+            "Stock Price Moving Averages",
+            "Iris Flower Classification",
+        ],
+        icons=["credit-card", "graph-up-arrow", "flower2"],
+        menu_icon="robot",
+        default_index=1,
     )
-if st.session_state.selected_model == "Moving Average":
+
+
+if selected == "Credit Card Fraud Detection":
+    supervised_learning.logistic_regression.credit_card_fraud_detection()
+
+if selected == "Stock Price Moving Averages":
     time_series_analysis.moving_average.artificial_use_case()
 
-if st.session_state.selected_model == "k-Nearest Neighbors (k-NN)":
+if selected == "Iris Flower Classification":
     supervised_learning.kth_nearest_neighbours.iris_flower_classification()
-
-if st.session_state.selected_model == "Logistic Regression":
-    supervised_learning.logistic_regression.credit_card_fraud_detection()
